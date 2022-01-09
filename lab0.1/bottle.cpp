8d8cff91e4d83f5d751e24666f8890257dbcc725
popovandrey25 <77118419+popovandrey25@users.noreply.github.com>
2022-01-09 16:45:59 +0300

    Update bottle.cpp

--- lab0.1/bottle.cpp
+++ lab0.1/bottle.cpp
@@ -1,23 +1,71 @@
 #include "bottle.h"
+#include <cmath>
 
-unsigned long long operator "" _tom3(unsigned long long littres) {
-   unsigned long long m3 = littres / 1000;
-   return m3;
+Bottle::Bottle() {
+    volume = 0.0;
+    filling = 0.0;
+    std:: cout << "Start: " << std:: endl;
 }
 
+Bottle::Bottle(double a, double b) {
+    if (a > 0.0 && b > 0.0 && b > 1){
+        volume = a;
+        filling = b;
+    }
+    else {
+        std:: cout << "Please enter correct numbers!" << std:: endl;
+    }
+    std:: cout << "The bottle according to your parameters has been created" << std:: endl;
+}
 
-int main(){
-   Bottle a;
-   std:: cin >> a;
-   Bottle b;
-   std:: cin >> b;
-   Bottle c = a + b;
-   std:: cout << c;
-   Bottle d;
-   std:: cin >> d;
-   Bottle f;
-   std:: cin >> f;
-   Bottle x = d - f;
-   std:: cout << x;
-   return 0;
+Bottle::Bottle(std::istream &is) {
+    std:: cout << "Please enter your bottle data: " << std:: endl;
+    is >>  volume >> filling;
+    if ((volume <= 0.0) || (filling <= 0.0) || (filling > 1)) {
+        std:: cout << "Invalind input. Enter again!" << std:: endl;
+        is >> volume >> filling;
+    }
+    std:: cout << "The bottle has been created via istream" << std:: endl;
+}
+
+void Bottle::Difference(const Bottle &c) {
+    double differ = volume*filling - c.volume*c.filling;
+    std:: cout << "Difference: " << differ << std:: endl;
+}
+
+void Bottle::Summa(const Bottle &c) {
+    double bottle = volume*filling + c.volume*c.filling;
+    std:: cout << "Total volume: " << bottle << std:: endl; 
+}
+
+void Bottle::Compare_volume(const Bottle &c) {
+    if (volume > c.volume){
+        std:: cout << "The first bottle is larger!" << std:: endl;
+    }
+    else if (volume == c.volume) {
+        std:: cout << "Volumes are equal!" << std:: endl;
+    }
+    else {
+        std:: cout << "The second bottle is larger!" << std:: endl;
+    }    
+}
+
+void Bottle::Compare_filling(const Bottle &c) {
+    if (volume*filling > c.volume*c.filling){
+        std:: cout << "The filling of first bottle is larger!" << std:: endl;
+    }
+    else if (volume*filling == c.volume*c.filling) {
+        std:: cout << "Bottles are equal!" << std:: endl;
+    }
+    else {
+        std:: cout << "The filling of second bottle is larger!" << std:: endl;
+    }    
+}
+
+void Bottle::Print(std::ostream& os) {
+    os << "Your bottle is: " << volume << ", " << filling << ", " << volume*filling << "l" << std:: endl;
+}
+
+Bottle::~Bottle() {
+    std:: cout << "FROM DESTRUCTOR: Your bottle has been deleted" << std:: endl;
 }
